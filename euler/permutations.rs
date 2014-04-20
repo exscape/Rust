c8084e465200b5831495e2a625b5efb0224c6199
Thomas Backman <serenity@exscape.org>
2014-04-20 02:34:32 +0200

    Clean up permutations.rs code, move some tests, move TODO

--- euler/permutations.rs
+++ euler/permutations.rs
@@ -1,23 +1,32 @@
+#![crate_id = "permutations#0.01"]
+
+//! Lexicographic permutations for `std::vec::Vec`.
+
 use std::{fmt, str};
 
-/// Mutates the vector to the next lexographical permutation.
+//
+// TODO: prev_permutation
+// TODO: try to avoid code duplication in the above
+// TODO: trait on Vec<T> instead of function
+// TODO: extensive testing
+// TODO: clean up code
+//
+
+/// Mutates the vector to the next lexicographical permutation.
 ///
 /// Returns `true` if successful, `false` if input is already at the last permutation.
 ///
 /// # Example
 ///
 /// ```rust
+/// use permutations::next_permutation;
 /// let mut v = vec!(0, 1, 2);
-/// permute(&mut v); // TODO
-/// assert_eq!(v, vec!(0, 2, 1);
-/// permute(&mut v);
-/// assert_eq!(v, vec!(1, 0, 2);
+/// next_permutation(&mut v);
+/// assert_eq!(v, vec!(0, 2, 1));
+/// next_permutation(&mut v);
+/// assert_eq!(v, vec!(1, 0, 2));
 /// ```
-///
-/// TODO: trait on Vec<T> instead of function
-/// TODO: extensive testing
-/// TODO: clean up code
-fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
+pub fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 	if v.len() < 2 { return false; } /* there is only 1 permutation each for these cases, so we are at the last */
 
 	/* 
@@ -27,20 +36,15 @@ fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 	let mut i : uint = 0;
 	let mut last : bool = true;
 
-//	println!("above loop; v.len = {}", v.len());
 	for x in range(1, v.len()).rev() {
-		i = x; // ffs
-//		println!("in loop: i = {}, v.len() = {}", i, v.len());
-		if v.get(i - 1) < v.get(i) { /*println!("found v[i-1] <v[i] ({} < {}) at i = {}", v.get(i-1), v.get(i), i); */last = false; break; }
+		i = x; // TODO!
+		if v.get(i - 1) < v.get(i) { last = false; break; }
 	}
 
 	if last {
 		/* This is the last permutation. */
-//		println!("last==true, returning false");
 		return false;
 	}
-
-//	println!("i = {}", i);
 
 	/*
 	 * Step 2:
@@ -50,13 +54,11 @@ fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 	let mut j : uint = 0;
 	let mut found : bool = false;
 	for x in range(i, v.len()).rev() {
-		j = x;
+		j = x; // TODO!
 		assert!(i > 0);
 		if v.get(j) > v.get(i-1) { found = true; break; }
 	}
 	assert_eq!(found, true);
-
-//	println!("i: {}, j: {}", i, j);
 
 	/* Step 3: Swap array[j] and array[i − 1]. */
 	v.as_mut_slice().swap(i-1, j);
@@ -69,6 +71,7 @@ fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 
 #[test]
 fn test_permutations() {
+	/* Test simple permutations */
 	let mut v : Vec<uint> = vec!(1, 2, 3, 4, 5);
 	next_permutation(&mut v);
 	assert_eq!(v, vec!(1, 2, 3, 5, 4));
@@ -78,26 +81,52 @@ fn test_permutations() {
 	assert_eq!(v, vec!(1, 2, 4, 5, 3));
 	next_permutation(&mut v);
 	assert_eq!(v, vec!(1, 2, 5, 3, 4));
-	// TODO: plenty of more tests, including empty, 1-length, 2-length etc. vectors of vastly different types
+
+	/* Test 0-length, 1-length and 2-length vectors */
+	let mut v : Vec<int> = Vec::new();
+	assert!(next_permutation(&mut v) == false);
+	assert_eq!(v, Vec::new());
+	v.push(1);
+	assert!(next_permutation(&mut v) == false);
+	assert_eq!(v, vec!(1));
+	v.push(2);
+	assert!(next_permutation(&mut v) == true);
+	assert_eq!(v, vec!(2, 1));
+	assert!(next_permutation(&mut v) == false);
+	assert_eq!(v, vec!(2, 1));
+
+	/* Test a custom struct */
+	struct TS { big: [uint, ..4], small: Option<uint> }
+	impl Ord for TS {
+		fn lt(&self, other: &TS) -> bool {
+			return self.small.unwrap() < other.small.unwrap();
+		}
+	}
+	impl Eq for TS {
+		fn eq(&self, other: &TS) -> bool {
+			return self.small.unwrap() == other.small.unwrap();
+		}
+	}
+	impl fmt::Show for TS {
+		fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+			write!(f.buf, "{}", self.small.unwrap())
+		}
+	}
+
+	let mut v2 : Vec<TS> = Vec::new();
+
+	for i in range(0u, 3) {
+		v2.push(TS { big: [i, ..4], small: Some(i) });
+	}
+
+	next_permutation(&mut v2);
+	assert_eq!(v2, vec!(TS{ big: [0, ..4], small: Some(0) }, TS{ big: [0, ..4], small: Some(2) }, TS{ big: [0, ..4], small: Some(1) }));
+	next_permutation(&mut v2);
+	assert_eq!(v2, vec!(TS{ big: [0, ..4], small: Some(1) }, TS{ big: [0, ..4], small: Some(0) }, TS{ big: [0, ..4], small: Some(2) }));
+
+	// TODO: what more can we test?
 }
 
-struct TestStruct { big: [uint, ..128], small: Option<uint> }
-
-impl Ord for TestStruct {
-	fn lt(&self, other: &TestStruct) -> bool {
-		return self.small.unwrap() < other.small.unwrap();
-	}
-}
-impl Eq for TestStruct {
-	fn eq(&self, other: &TestStruct) -> bool {
-		return self.small.unwrap() == other.small.unwrap();
-	}
-}
-impl fmt::Show for TestStruct {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f.buf, "{}", self.small.unwrap())
-	}
-}
 
 fn main() {
 	let mut v : Vec<uint> = vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
@@ -110,17 +139,6 @@ fn main() {
 	assert_eq!(v, vec!(2, 7, 8, 3, 9, 1, 5, 4, 6, 0));
 	println!("{}", v);
 
-	let mut v2 : Vec<TestStruct> = Vec::new();
-
-	for i in range(0u, 3) {
-		v2.push(TestStruct { big: [0, ..128], small: Some(i) });
-	}
-
-	println!("Fat struct:");
-	println!("{}", v2);
-	while next_permutation(&mut v2) {
-		println!("{}", v2);
-	}
 
 	let s = "ABCD";
 	let mut t = s.chars().collect::<Vec<char>>();
