1f56463838688b76b90a2e4d15a00b9025fa02d3
Thomas Backman <serenity@exscape.org>
2014-04-22 15:00:19 +0200

    permutations.rs: cleanups, add benchmarks, etc. Much more work needed

--- euler/permutations.rs
+++ euler/permutations.rs
@@ -2,8 +2,12 @@
 
 //! Lexicographic permutations for `std::vec::Vec`.
 
-use std::{fmt, str};
+extern crate std;
+extern crate test;
 
+//
+// TODO: first of all, clean up the code
+//
 //
 // TODO: prev_permutation
 // TODO: try to avoid code duplication in the above
@@ -12,9 +16,10 @@ use std::{fmt, str};
 // TODO: clean up code
 //
 
-/// Mutates the vector to the next lexicographical permutation.
+/*
+/// Mutates the vector to the next lexicographic permutation.
 ///
-/// Returns `true` if successful, `false` if input is already at the last permutation.
+/// Returns `true` if successful, `false` if input is already at the last permutation. TODO
 ///
 /// # Example
 ///
@@ -26,6 +31,32 @@ use std::{fmt, str};
 /// next_permutation(&mut v);
 /// assert_eq!(v, vec!(1, 0, 2));
 /// ```
+*/
+
+pub struct LexicographicPermutations<T> {
+	v: Vec<T>
+}
+
+trait LexicographicPermutationsTrait<T: Clone> { // TODO: rename
+	fn lexicographic_permutations(self) -> LexicographicPermutations<T>;
+}
+
+impl<'a, T: Clone + Ord> LexicographicPermutationsTrait<T> for &'a [T] {
+	fn lexicographic_permutations(self) -> LexicographicPermutations<T> {
+		LexicographicPermutations { v: Vec::from_slice(self) }
+	}
+}
+
+impl<T: Clone + Ord> Iterator<Vec<T>> for LexicographicPermutations<T> {
+	fn next(&mut self) -> Option<Vec<T>> {
+		let ret = self.v.clone();
+		match next_permutation(&mut self.v) {
+			true => Some(ret),
+		 	false => None // TODO: this misses the very last permutation
+		}
+	}
+}
+
 pub fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 	if v.len() < 2 { return false; } /* there is only 1 permutation each for these cases, so we are at the last */
 
@@ -33,12 +64,12 @@ pub fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 	 * Step 1: 
      * Find largest index i such that array[i−1] < array[i]. If no such index exists, the permutation is the last permutation.
 	 */
-	let mut i : uint = 0;
 	let mut last : bool = true;
 
-	for x in range(1, v.len()).rev() {
-		i = x; // TODO!
+	let mut i : uint = v.len() - 1;
+	while i > 0 {
 		if v.get(i - 1) < v.get(i) { last = false; break; }
+		i -= 1;
 	}
 
 	if last {
@@ -50,15 +81,12 @@ pub fn next_permutation<T : Ord>(v: &mut Vec<T>) -> bool {
 	 * Step 2:
 	 * Find largest index j such that j ≥ i and array[j] > array[i − 1].
 	 */
-	assert!(v.get(i-1) < v.get(i)); // pivot must be smaller than first part of suffix
-	let mut j : uint = 0;
-	let mut found : bool = false;
-	for x in range(i, v.len()).rev() {
-		j = x; // TODO!
-		assert!(i > 0);
-		if v.get(j) > v.get(i-1) { found = true; break; }
+	let mut j = v.len() - 1;
+	assert!(i > 0);
+	while j > 0 {
+		if v.get(j) > v.get(i-1) { break; }
+		j -= 1;
 	}
-	assert_eq!(found, true);
 
 	/* Step 3: Swap array[j] and array[i − 1]. */
 	v.as_mut_slice().swap(i-1, j);
@@ -107,8 +135,8 @@ fn test_permutations() {
 			return self.small.unwrap() == other.small.unwrap();
 		}
 	}
-	impl fmt::Show for TS {
-		fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	impl std::fmt::Show for TS {
+		fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
 			write!(f.buf, "{}", self.small.unwrap())
 		}
 	}
@@ -124,27 +152,42 @@ fn test_permutations() {
 	next_permutation(&mut v2);
 	assert_eq!(v2, vec!(TS{ big: [0, ..4], small: Some(1) }, TS{ big: [0, ..4], small: Some(0) }, TS{ big: [0, ..4], small: Some(2) }));
 
-	// TODO: what more can we test?
-}
-
-
-fn main() {
+	// Project euler #24
 	let mut v : Vec<uint> = vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-	println!("PERMUTATION: {}", v);
 
 	for _ in range(0, 999999) {
 		 next_permutation(&mut v);
 	}
 	assert_eq!(v, vec!(2, 7, 8, 3, 9, 1, 5, 4, 6, 0));
-	println!("{}", v);
-
 
 	let s = "ABCD";
 	let mut t = s.chars().collect::<Vec<char>>();
-	println!("String:");
-	println!("{}", s);
-	while next_permutation(&mut t) {
-		println!("{}", str::from_chars(t.as_slice()));
+	next_permutation(&mut t);
+	assert_eq!(t, vec!('A', 'B', 'D', 'C'));
+	next_permutation(&mut t);
+	assert_eq!(t, vec!('A', 'C', 'B', 'D'));
+	next_permutation(&mut t);
+	assert_eq!(t, vec!('A', 'C', 'D', 'B'));
+}
+
+#[bench]
+fn bench_without_iterator(b: &mut test::test::Bencher) {
+	let mut v = vec!(0, 1, 2, 3, 4, 5, 6, 7);
+	b.iter(|| { while next_permutation(&mut v) { } });
+}
+
+#[bench]
+fn bench_with_iterator(b: &mut test::test::Bencher) {
+	let v = vec!(0, 1, 2, 3, 4, 5, 6, 7);
+	b.iter(|| { for _ in v.as_slice().lexicographic_permutations() { } });
+}
+
+#[allow(dead_code)]
+fn main() {
+	let mut v = vec!(1,2,3, 4, 5);
+
+	loop {
+		println!("{} ", v);
+		if !next_permutation(&mut v) { break; }
 	}
 }
