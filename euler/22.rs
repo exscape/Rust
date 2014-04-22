afd415827e2781389f818236c89f2acba6cf6373
Thomas Backman <serenity@exscape.org>
2014-04-22 16:21:54 +0200

    Modify #22 to use enumerate(). That probably made it harder to read, though...

--- euler/22.rs
+++ euler/22.rs
@@ -5,11 +5,6 @@ use std::io::fs::File;
 use std::io::BufferedReader;
 use std::iter::AdditiveIterator;
 
-fn score(name: &str, pos: uint) -> uint {
-	/* pos is 0-indexed, so we need to add 1; similarly, 'A' - 'A' is 0, so we need to add 1 there, too */
-	(pos + 1) * name.chars().map(|c| (c as uint) - ('A' as uint) + 1).sum()
-}
-
 fn main() {
 	let reader = File::open(&Path::new("22.txt")).unwrap();
 	let mut reader = BufferedReader::new(reader);
@@ -18,11 +13,11 @@ fn main() {
 	let mut names = s.split(',').map(|s| s.trim_chars(&'"')).collect::<Vec<&str>>();
 	names.sort();
 
-	let mut sum = 0;
-
-	for i in range(0, names.len()) {
-		sum += score(*names.get(i), i);
-	}
+	/* pos is 0-indexed, so we need to add 1; similarly, 'A' - 'A' is 0, so we need to add 1 there, too */
+	let sum = names.iter().enumerate().map(|(pos, n)| {
+	          n.chars().map(|c| (c as uint) - ('A' as uint) + 1).sum() * (pos + 1) /* this sum is letter values */
+			  })
+			  .sum(); /* and this sum is for the full name scores to the final sum */
 
 	println!("Answer: {}", sum);
 }
