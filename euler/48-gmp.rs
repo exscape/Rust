b1a02bd692e1cd8131eb0f4e38b067c8ddc13eba
Thomas Backman <serenity@exscape.org>
2014-05-13 21:16:56 +0200

    Add euler #48, using rust-gmp

---
+++ euler/48-gmp.rs
@@ -0,0 +1,15 @@
+extern crate gmp;
+use std::iter::range_inclusive;
+use gmp::Mpz;
+
+fn main() {
+	static LIMIT : uint = 1000;
+	let mut sum = Mpz::new();
+
+	for i in range_inclusive(1u, LIMIT) {
+		sum = sum + std::num::pow(std::num::from_uint(i).unwrap(), i);
+	}
+
+	let sum_str = sum.to_str();
+	println!("{}", sum_str.slice_from(sum_str.len() - 10));
+}
